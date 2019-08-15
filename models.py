e52495ee2624491ba00e5edf473298474100cb48
ingecraft <ingenious.craftsman@gmail.com>
2019-08-15 15:48:35 +0000

    Added Charges model

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float, Boolean
 from sqlalchemy.orm import relationship
 
 Base = declarative_base()
@@ -12,19 +12,18 @@ class Customer(Base):
     surname = Column(String)
     email = Column(String)
     date_created = Column(Date)
-    recurring_gifts = relationship("RecurringGift")
-
-
-class RecurringGift(Base):
-    __tablename__ = "reccuring_gift"
-
-    id = Column(Integer, primary_key=True)
-    date_created = Column(Date)
-    customer_id = Column(Integer, ForeignKey('customer.id')
-    
-
+    charges = relationship("Charge")
 
 class Charge(Base):
     __tablename__ = 'charge'
+    
+    date_created = Column(Date)
+    amount = Column(Float)
+    is_recurring = Column(Boolean)
+    is_refunded = Column(Boolean)
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+
+    
+
     
 
