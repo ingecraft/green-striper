2c57e5bc60467861b9aa5fda6a54e4e38e947139
ingecraft <ingenious.craftsman@gmail.com>
2019-08-19 20:46:19 +0000

    Added session to db connector

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from models import Base
 
@@ -7,7 +8,12 @@ class DBConnector():
     def __init__(self, config):
         self.engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
         self.base = Base
+        Session = sessionmaker(bind=self.engine)
+        self._session = Session()
 
+    @property
+    def session(self):
+        return self._session 
 
 def initiate_db(config):
     db_conn = DBConnector(config)
--- tests.py
+++ tests.py
@@ -2,9 +2,9 @@ import unittest
 
 import db
 import config
+from sqlalchemy.orm.session import Session
 
 class TestDatabase(unittest.TestCase):
-    
 
     def setUp(self):
         self.configuration = config.config['testing']
@@ -13,6 +13,10 @@ class TestDatabase(unittest.TestCase):
     def test_db_initialization(self):
         db.initiate_db(self.configuration)
         self.assertEqual(1,1)
+
+    def test_new_session(self):
+        session = self.db_conn.session
+        self.assertIsInstance(session, Session) 
 
 if __name__ == '__main__':
     unittest.main()
