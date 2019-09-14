3bd2b54dec330fe60651b9bcb02d0c2c93ee7eb8
ingecraft <ingenious.craftsman@gmail.com>
2019-09-14 18:35:18 +0300

    Added test for stripe customers

--- api_connection.py
+++ api_connection.py
@@ -9,17 +9,19 @@ class StripeAPI():
     default_start_date = datetime.datetime(2018,1,1)
     default_end_date = datetime.datetime.now()
 
-    def __init__(self, config):
+    def __init__(self, key):
         client = stripe.http_client.RequestsClient()
         self._api_client = stripe
         self._api_client.default_http_client = client
-        self._api_client.api_key = config 
+        self._api_client.api_key = key
 
     @property
     def api_client(self):
         return self._api_client
 
-    def customers_by_date(self, start_date=default_start_date, end_date=default_end_date):
+    def customers_by_date(self, start_date=default_start_date,
+            end_date=default_end_date):
         unix_start = aux.datetime_to_unix(start_date)
-        unix_end = aux.datetime_unix(end_date)
-        pass
+        unix_end = aux.datetime_to_unix(end_date)
+         
+
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 import unittest
+import datetime
+
 import stripe
 
 import db
@@ -27,10 +29,17 @@ class TestAPIConnection(unittest.TestCase):
 
     def setUp(self):
         self.api_conn = ac.StripeAPI(test_config)
+        self.stripe_client = self.api_conn.api_client
 
     def test_stripe_client(self):
         api_client = self.api_conn.api_client
         self.assertIs(api_client, stripe)
+
+    def test_customers_valid_range(self):
+        start_date = datetime.datetime(2018,1,1)
+        end_date = datetime.datetime.now()
+        customers = self.api_conn.customers_by_date(start_date, end_date)
+        self.assertIsNotNone(customers)
             
 
 if __name__ == '__main__':
